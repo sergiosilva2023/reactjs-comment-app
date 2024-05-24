1de2512646db3517ee1301ba56554942e91e1fc6
Sérgio Silva <sergio.web100@gmail.com>
2024-05-24 20:24:16 +0100

    Projeto React Componente Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,13 @@
+import "./Comment.css"
+
+const Comment = () => {
+    return (
+        <div className="comment">
+        <h2>Apresentação...</h2>
+        <p>O Sérgio é uma brasa!</p>
+        <p>17/05/1977</p>
+        </div>
+    )
+}
+
+export default Comment;
