c25dfc2cdc3b1b90cc39b4024e1d4a1301df4248
Sérgio Silva <sergio.web100@gmail.com>
2024-05-26 11:56:35 +0100

    Migrei o App Componente para Class Componente e implementado o State com maps.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
+import React from "react";
 import './App.css';
 import Comment from "./components/Comment";
-function App() {
-  return (
-    <div className="App">
-      <h1>Comentário - Opinião</h1>
-      <Comment
-          name="Sérgio Silva"
-          email="sergio.web100@gmail.com"
-          date={new Date()}
-          message="Estou gordo"
-          />
-      <Comment 
-           name="Wiliam Silva"
-           email="wiliam_2004@sapo.pt"
-           date={new Date()}
-           message="Puto fixe"
-           />
+
+class App extends React.Component {
+
+
+  state = {
+    comments : [
+      {
+        name: 'Sérgio Silva',
+        email: 'sergio.web100@gmail.com',
+        date: new Date(),
+        message: 'Estou gordo',
+      },
+      {
+        name: 'Wiliam Silva',
+        email: 'wiliam_2004@sapo.pt',
+        date: new Date(),
+        message: 'Estou lindo'
+      },
+    ],
+  };
+
+  addComment=()=>{
+    const newComment = {
+      name: 'Sónia Loureiro',
+      email: 'sonia.web100@gmail.com',
+      date: new Date(),
+      message: 'Sou muito linda'
+    };
+
+    this.setState({
+      comments: [...this.state.comments, newComment]
+    });
+
+
+//    this.state.comments.push(newComment)
+    alert('Devo adiconar um comentário')
+  };
+
+    render() {
+      return (
+      <div className="App">
+      <h1>My Comments App!</h1>
+      {this.state.comments.map((comment, index)=>{
+        return(
+        <Comment
+        key={index}
+        name={comment.name}
+        email={comment.email}
+        date={comment.date}
+        message={comment.message}
+        />
+      );
+      })}
+      <button onClick={this.addComment}>Adicionar Comentário.</button>
     </div>
-  );
+      );
+    }
 }
 
 export default App;
